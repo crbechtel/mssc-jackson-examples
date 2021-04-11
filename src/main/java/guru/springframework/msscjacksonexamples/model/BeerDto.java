1dbc9f9a41061817c1edcee52063795167cb7d6e
cliffordbechtel <cliffordbechtel@cliffords-mini>
2021-04-11 16:04:44 -0400

    custome deserializer with Jackson

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.Positive;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -43,11 +44,12 @@ public class BeerDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
     
-    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     
     private OffsetDateTime lastUpdatedDate;
     
     @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate myLocalDate;
 }
---
+++ src/main/java/guru/springframework/msscjacksonexamples/model/LocalDateDeserializer.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package guru.springframework.msscjacksonexamples.model;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+/**
+ * Created by @author cliffordbechtel on Apr 11, 2021
+ *
+ */
+public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+
+	public LocalDateDeserializer() {
+		super(LocalDate.class);
+	}
+	
+	@Override
+	public LocalDate deserialize(JsonParser p, DeserializationContext ctxt)
+			throws IOException, JsonProcessingException {
+		return LocalDate.parse(p.readValueAs(String.class), DateTimeFormatter.BASIC_ISO_DATE);
+	}
+}
